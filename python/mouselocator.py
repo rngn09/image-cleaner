35d6473d65caf4a8c98ecc134796d6d8ff1bf967
rngn <rabiaengin@gmail.com>
2020-05-21 16:10:26 -0400

    add mouselocator

---
+++ python/mouselocator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import pyautogui
+
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')
